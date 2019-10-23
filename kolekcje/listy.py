a2e090c102dda2e760ffafc76f5030039f694d0a
WajuHCC <WajuHCC>
2019-10-23 20:30:52 +0200

    zmiany w skrypcie 2

---
+++ kolekcje/listy.py
@@ -0,0 +1,10 @@
+my_list=["jeden",1, "dwa", 2, "ala", "ola"]
+print(my_list)
+print(my_list[2])
+
+my_list2= [1, 2, 3, my_list]
+print(my_list2[3][1])
+print(dir(my_list2))
+my_list.append(10)
+print(my_list)
+
