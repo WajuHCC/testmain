c33489631d6af99e4db30837f8fbfca5d52ffe28
WajuHCC <WajuHCC>
2019-10-23 18:23:49 +0200

    porządkowanie plików

---
+++ arytmetyka.py
@@ -0,0 +1,19 @@
+x = 3
+y = 4
+print("dodawanie", x + y)
+print("odejmowanie", x - y)
+print("mnozenie", x * y)
+print("dzielenie", x / y)
+print("dzelenie calkowite", x // y)
+print("reszta z dzielenia", x % y)
+print("potegowanie", x ** y)
+print()
+x = 9
+y = 4
+print("dodawanie", x + y)
+print("odejmowanie", x - y)
+print("mnozenie", x * y)
+print("dzielenie", x / y)
+print("dzelenie calkowite", x // y)
+print("reszta z dzielenia", x % y)
+print("potegowanie", x ** y)
---
+++ typy_liczbowe.py
@@ -0,0 +1,24 @@
+#liczby całkowite - intereger
+print(type(10))
+print(int())
+print(int("10"))
+print(int("10",base=3))
+print(int(2.7))
+
+#liczby zmiennoprzecinkowe - float
+print(float())
+print(float(1/3))
+print(0.1 + 0.1 + 0.1)
+print(0.1 + 0.1 + 0.1 == 0.3)
+
+#liczby zespolone: complex
+print(complex())
+print(10 + 10j)
+
+#wartośći boolowskie: bool True False
+print(bool())
+print(10 + True)
+print(True and False)
+print(not(True))
+print(True or False)
+print(not(True))
