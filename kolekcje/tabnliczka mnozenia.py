87403dd39f6233bfbfae6c887d8f32d91b988240
WajuHCC <WajuHCC>
2019-10-28 20:28:05 +0100

    zmiany w skrypcie 2

---
+++ kolekcje/tabnliczka mnozenia.py
@@ -0,0 +1,12 @@
+for i in range(5):
+    print(i)
+
+for i in range(5):
+    print(i, end="")
+print(40*"_")
+
+for i in range(5):
+    for j in range(5):
+        x = i * j
+        print(x)
+        print(f{})
