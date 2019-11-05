5cd05d25d8600fe41e1135bd5cc376005def8b07
WajuHCC <56654671+WajuHCC@users.noreply.github.com>
2019-11-05 20:51:36 +0100

    Update moja_szachownica.py

--- funkcje/moja_szachownica.py
+++ funkcje/moja_szachownica.py
@@ -8,9 +8,14 @@ setheading
 270 - south
 
 """
+f = int(input("Aby narysowac szachownice, wpisz liczbe żądanych pol (minimum 2): "))
+if f <2:
+    print("SERIO?! Szachownica z jednym polem?")
+    quit()
+
 turtle.speed(0)
 
-
+#empty box
 def empty_box(steps=60):
     turtle.setheading(270)
     turtle.forward(steps)
@@ -23,9 +28,7 @@ def empty_box(steps=60):
     turtle.setheading(0)
     turtle.forward(steps)
 
-
-# empty_box()
-
+# full_box()
 def full_box(steps=60):
     for a in range(30):
         turtle.setheading(270)
@@ -37,43 +40,40 @@ def full_box(steps=60):
         turtle.setheading(0)
         turtle.forward(1)
 
-
-# full_box()
-def full_line_odd(f): #linia nieparzysta, f oznacza liczbę pól szachownicy
+# whole_line()
+def whole_line_odd(f): #linia nieparzysta, f oznacza liczbę pól szachownicy
     for b in range(f // 2):
-        if f >= 4:
-            empty_box()
-            full_box()
-        else:
-            empty_box()
+        empty_box()
+        full_box()
 
-
-def full_line_even(f): #linia parzysta
+def whole_line_even(f): #linia parzysta
     for b in range(f // 2):
-        if f >= 4:
-            full_box()
-            empty_box()
-        else:
-            full_box()
+        full_box()
+        empty_box()
 
 def back_to_start(f,steps=60): #nawrotka
-    if f % 2 > 0:
+    if f % 2 > 0 and f >= 4:
         turtle.setheading(180)
-        turtle.forward(f//2*2*steps) #to trzeba poprawic zweby nawrotki przy nieparzystych sie zgadzaly
+        turtle.forward(f//2*2*steps)
+        turtle.setheading(270)
+        turtle.forward(steps)
+    elif f % 2 ==0 and f >=4:
+        turtle.setheading(180)
+        turtle.forward(f * steps)
         turtle.setheading(270)
         turtle.forward(steps)
     else:
         turtle.setheading(180)
-        turtle.forward(f * steps)  # to trzeba poprawic zweby nawrotki przy nieparzystych sie zgadzaly
+        turtle.forward(2*steps)
         turtle.setheading(270)
         turtle.forward(steps)
 
 def szachownica(f):
     for c in range(f//2):
-        full_line_odd(f)
+        whole_line_odd(f)
         back_to_start(f)
-        full_line_even(f)
+        whole_line_even(f)
         back_to_start(f)
 
-szachownica(4)
+szachownica(f)
 turtle.done()
