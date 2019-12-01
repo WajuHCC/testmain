668d19df3b545288f26af28e823e731e9c5beae2
WajuHCC <56654671+WajuHCC@users.noreply.github.com>
2019-12-01 23:52:04 +0100

    pythonbook while loops

--- praca_z_plikami/analiza_logow.py
+++ praca_z_plikami/analiza_logow.py
@@ -2,19 +2,35 @@
 otworz plik - czytaj plik
 1. utworz 2 slowniki Login i Logout
 """
-# def login_time(input_file):
-logins = {}
-logouts = {}
-with open("logs.txt") as f:
-    for rekord in f:
-        lista = rekord.split(";")
-        if lista[1] == "LOGIN":
-            logins.update({lista[0]:lista[2]})
-        else:
-            logouts.update({lista[0]:lista[2]})
-
-    print(logins)
-    print(logouts)
 
 
-# login_time(dane/logs.txt)
+def login_time(user):
+    user_logins = []
+    user_logouts = []
+    with open("logs.txt") as f:
+        for rekord in f:
+            lista = rekord.split(";")
+            if lista[0] == user and lista[1] == "LOGIN":
+                user_logins.append(int(lista[2]))
+
+            elif lista[0] == user and lista[1] == "LOGOUT":
+                user_logouts.append(int(lista[2]))
+        total_log = sum(user_logouts) - sum(user_logins)
+        return print("Całowity czas zalogowania uzytkownika: " + user + " wynosi " + str(total_log))
+
+
+def all_login_times(input_file):
+    user_set = set()
+    with open(input_file) as g:
+        for rekord in g:
+            user_set.add(rekord[0:6])
+    user_names = sorted(user_set)
+    for user in user_names:
+        login_time(user)
+    return
+
+
+login_time("user-1")
+login_time("user-2")
+print()
+all_login_times("logs.txt")
